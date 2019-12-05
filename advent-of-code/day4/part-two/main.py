05eb0b06cd19707bab21a7920773fa4093066445
Shubha Rajan <shubharajan94@gmail.com>
2019-12-05 03:41:18 -0800

    Add files via upload

---
+++ advent-of-code/day4/part-two/main.py
@@ -0,0 +1,18 @@
+
+from collections import Counter
+def check_num(num):
+    if num != "".join(sorted(num)): return False
+    counts = Counter(num)
+    for value in counts.values():
+        if value == 2: return True
+    return False
+
+def possible_passwords(min, max):
+    count = 0
+    for i in range(min, max):
+        if check_num(str(i)):
+            count += 1
+    return count
+
+if __name__ == "__main__":
+    print(possible_passwords(158126,624574))
